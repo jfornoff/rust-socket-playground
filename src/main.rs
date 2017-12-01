c9aa5d8cb9827539c8f4126217e9817820c6a0e0
Jan Fornoff <jan.fornoff@grandcentrix.net>
2017-12-01 17:34:41 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::net::{TcpListener, TcpStream};
+use std::os::unix::io::AsRawFd;
+
+fn main() {
+    let listener = TcpListener::bind("0.0.0.0:80").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("NEW CONNECTION");
+                output_info(stream)
+            }
+            Err(_e) => println!("We dun goofd :-("),
+        }
+    }
+}
+
+fn output_info(stream: TcpStream) {
+    println!(
+        "Local address: {:?}",
+        stream.local_addr().expect(
+            "Didn't get the local address :-(",
+        )
+    );
+
+    println!(
+        "Remote address: {:?}",
+        stream.peer_addr().expect(
+            "Didn't get the remote address :-(",
+        )
+    );
+
+    println!("File descriptor {:?}", stream.as_raw_fd());
+}
